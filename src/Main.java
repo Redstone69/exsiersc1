54c0494772f5496bd69b0ebcc768cc3ec5bfc778
HazemCSS <148874705+HazemCSS@users.noreply.github.com>
2025-01-23 14:22:53 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+
+        double[] temps = {23,23,23,23,23};
+        
+
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(temps[i]);
+            double total =
+        }
+
+
+        String city_name = "los santos";
+        int zippp = 323;
+        float temp = (97+96+95+93+100)/5;
+        System.out.println("the city is ");
+        System.out.println(city_name);
+        System.out.println("the zip code is ");
+        System.out.println(zippp);
+        System.out.println("the temp is ");
+        System.out.println(temp);
+
+
+    }
+}
