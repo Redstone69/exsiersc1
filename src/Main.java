cf00ef887f0b52bc64c65003b6b1c61bd4cd0ac7
Redstone69 <89121962+Redstone69@users.noreply.github.com>
2025-01-26 12:52:27 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!");
-
-        double[] temps = {23,23,23,23,23};
-        
-
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(temps[i]);
-            double total =dfsdf;
+        double[] temps = {93,91,100,81,97};
+        int sum = 0;
+        int i;
+        for (i = 0; i < temps.length; i++) {
+            sum += temps[i];
         }
+
 
 
         String city_name = "los santos";
         int zippp = 323;
-        float temp = (97+96+95+93+100)/5;
+        float temp = (sum)/5;
         System.out.println("the city is ");
         System.out.println(city_name);
         System.out.println("the zip code is ");
